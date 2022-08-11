7ea58425aad1a4d55d3820d52f0f23383df6d522
KruzadeR-VictoR <victorbiju31@gmail.com>
2022-08-11 20:34:35 +0530

    fixed email validation

---
+++ script.js
@@ -0,0 +1,15 @@
+const error = document.querySelector(".error");
+
+const validate = (event) => {
+  console.log(event);
+  console.log(event.target.value);
+  if (event.target.value == "") {
+    error.innerHTML = "";
+    subError.innerHTML = "";
+  } else if (event.target.value.includes("@" && ".")) {
+    error.innerHTML = "";
+    subError.innerHTML = "";
+  } else {
+    error.innerHTML = "enter a valid email id";
+  }
+};
